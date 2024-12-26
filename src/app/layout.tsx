a69480eb9a7f64d4535ba0a07128ca6f4278fc1c
Yahya Alazzi <yahyaalazzi@Yahyas-MacBook-Air.local>
2024-12-26 18:12:55 +0300

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,22 +21,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="title" content="Ezzo" />
         <meta
           name="description"
-          content="اكتشف أفضل المؤشرات للتحليل الفني للتداول، واحصل على أدوات ومؤشرات عالية الدقة لمساعدتك في اتخاذ قرارات التداول بثقة."
+          content="اكتشف أفضل المؤشرات للتحليل الفني للتداول..."
         />
         <meta
           name="keywords"
-          content="مؤشر تداول, تحليلات فنية, أدوات تداول, سوق الأسهم, التداول, مؤشرات تقنية"
+          content="مؤشر تداول, تحليلات فنية, أدوات تداول..."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MQRDBB3V');`}
+          {`(function(w,d,s,l,i){...})(window,document,'script','dataLayer','GTM-MQRDBB3V');`}
         </Script>
         {/* End Google Tag Manager */}
 
